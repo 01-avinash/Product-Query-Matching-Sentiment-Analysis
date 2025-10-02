879be1ef45eff97578abb5483e87928763840427
01-avinash <153107388+01-avinash@users.noreply.github.com>
2025-10-02 10:30:02 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,10 @@
+Enter your search query: good battery with good quality
+Query Sentiment → Positive (score=1.2017)
+================================================================================
+Final Results for Query: 'good battery with good quality'
+================================================================================
+| Product Name     |   Price |   Rating |   Match Score | Sentiment Summary   |   Sentiment Score |
+|:-----------------|--------:|---------:|--------------:|:--------------------|------------------:|
+| Redmi Note 10    | 1426.46 |     3.40 |        0.3837 | Positive            |            1.6630 |
+| Dell Inspiron 15 |  187.07 |     3.10 |        0.2772 | Positive            |            0.4500 |
+| Acer Aspire 7    |  503.99 |     3.70 |        0.0875 | Positive            |            0.8744 |
