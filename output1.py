fba7782d144efb4b6b4cc4e05ec3aeae8f04c77f
01-avinash <153107388+01-avinash@users.noreply.github.com>
2025-10-02 10:32:47 +0530

    Add files via upload

---
+++ output1.py
@@ -0,0 +1,10 @@
+Enter your search query: bad phone drains fast
+Query Sentiment → Negative (score=-0.8839)
+================================================================================
+Final Results for Query: 'bad phone drains fast'
+================================================================================
+| Product Name     |   Price |   Rating |   Match Score | Sentiment Summary   |   Sentiment Score |
+|:-----------------|--------:|---------:|--------------:|:--------------------|------------------:|
+| Boat Rockerz 450 | 1233.18 |     3.50 |        0.4783 | Negative            |           -1.2348 |
+| Realme 8 Pro     |  821.86 |     3.40 |        0.4491 | Negative            |           -0.8167 |
+| MacBook Air M1   | 1307.80 |     3.40 |        0.2680 | Negative            |           -0.8662 |
