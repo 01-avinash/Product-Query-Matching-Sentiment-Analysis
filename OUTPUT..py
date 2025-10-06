244ea1b975bbda5a55ef4675a52d8eafb32d8b6d
01-avinash <153107388+01-avinash@users.noreply.github.com>
2025-10-06 10:17:58 +0530

    Add files via upload

---
+++ OUTPUT..py
@@ -0,0 +1,81 @@
+1.Enter your search query: i want a best laptop for gaming 
+Query Sentiment → Positive (score=1.1314)
+Detected Category: Laptops
+====================================================================================================
+| Product Name     |   Price |   Rating |          Match Score | Sentiment Summary   |   Sentiment Score |
+|:-----------------|--------:|---------:|---------------------:|:--------------------|------------------:|
+| Acer Aspire 7    |  503.99 |   3.7000 | 0.047576898580322095 | Positive            |            1.2719 |
+| Dell Inspiron 15 |  187.07 |   3.1000 | 0.04789436786497554  | Positive            |            0.5506 |
+| HP Pavilion      | 1257.35 |   2.9000 | 0.059541730758464015 | Positive            |            0.1443 |
+
+
+
+
+2.Enter your search query:suggest me a laptop with good sound 
+Query Sentiment → Positive (score=0.6718)
+Detected Category: Laptops
+====================================================================================================
+| Product Name     |   Price |   Rating |          Match Score | Sentiment Summary   |   Sentiment Score |
+|:-----------------|--------:|---------:|---------------------:|:--------------------|------------------:|
+| Acer Aspire 7    |  503.99 |   3.7000 | 0.21334088312052588  | Positive            |            1.2719 |
+| Dell Inspiron 15 |  187.07 |   3.1000 | 0.21476445589581436  | Positive            |            0.5506 |
+| HP Pavilion      | 1257.35 |   2.9000 | 0.043949845344803014 | Positive            |            0.1443 |
+
+
+
+
+3.Enter your search query: suggest me a laptop for dailyuse 
+Query Sentiment → Neutral (score=0.0)
+Detected Category: Laptops
+====================================================================================================
+| Product Name    |   Price |   Rating |         Match Score | Sentiment Summary   |   Sentiment Score |
+|:----------------|--------:|---------:|--------------------:|:--------------------|------------------:|
+| Lenovo ThinkPad | 1980.09 |   4.1000 | 0.15039539131518206 | Positive            |            0.0775 |
+| HP Pavilion     | 1257.35 |   2.9000 | 0.14675485798602114 | Positive            |            0.1443 |
+| Asus ROG        | 1661.42 |   4.5000 | 0.115008263668454   | Negative            |           -0.5143 |
+
+
+
+
+4.Enter your search query: list mobiles  camera
+Query Sentiment → Neutral (score=0.0)
+Detected Category: Mobiles
+====================================================================================================
+| Product Name   |   Price |   Rating |        Match Score | Sentiment Summary   |   Sentiment Score |
+|:---------------|--------:|---------:|-------------------:|:--------------------|------------------:|
+| OnePlus Nord   | 1243.84 |   3.9000 | 0.0                | Positive            |            0.0655 |
+| iPhone 13      |  265.18 |   3.6000 | 0.2451682756608853 | Positive            |            0.8285 |
+| Realme 8 Pro   |  821.86 |   3.4000 | 0.2398294674675247 | Negative            |           -1.2618 |
+
+
+
+
+5.Enter your search query: suggest some smartwatches which has best battery 
+Query Sentiment → Positive (score=1.1314)
+====================================================================================================
+| Product Name           |   Price |   Rating |         Match Score | Sentiment Summary   |   Sentiment Score |
+|:-----------------------|--------:|---------:|--------------------:|:--------------------|------------------:|
+| Samsung Galaxy Watch 4 |  312.55 |   3.4000 | 0.0                 | Positive            |            2.8619 |
+| Redmi Note 10          | 1426.46 |   3.4000 | 0.15848858575312605 | Positive            |            2.298  |
+| Galaxy M21             | 1314.91 |   2.6000 | 0.27410753369487245 | Positive            |            2.0647 |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
